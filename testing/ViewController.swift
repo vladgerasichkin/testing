7bf44089bda45fe0a3329aaaaa1b8861af9aec72
Vlad Gerasichkin <vladgerasichkin@gmail.com>
2018-10-14 18:06:24 +0200

    nothing added

--- testing/ViewController.swift
+++ testing/ViewController.swift
@@ -12,9 +12,15 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print(firstFunc(5, 1))
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    func firstFunc(_ number: Int, _ number2: Int = 0) -> Int {
+        let sum = number + number2
+        return sum
+    }
+    func foo() {
+        print("nothing")
+    }
 }
 
