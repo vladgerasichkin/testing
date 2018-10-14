bc2c6224744939e8bc41ca97001885b0c4e988f5
Vlad Gerasichkin <vladgerasichkin@gmail.com>
2018-10-14 16:13:35 +0200

    Initial Commit

---
+++ testing/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testing
+//
+//  Created by Влад Герасичкин on 14.10.2018.
+//  Copyright © 2018 Влад Герасичкин. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
